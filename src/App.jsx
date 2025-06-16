74d8969cb10c8200370ea59cb1cf97e46b0511a9
AmanR404 <107179665+AmanR404@users.noreply.github.com>
2025-06-16 15:30:22 +0530

    Final Tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './Components/Footer'
 import Dashboard from './Pages/Product Catalogue/Dashboard'
 import Cart from './Pages/Cart/Cart'
 import Wishlist from './Pages/Wishlist/Wishlist'
+import Details from './Pages/Order Details/Details'
+import Success from './Pages/Order Success/Success'
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/catalogue' element={<><Header/><Dashboard/><Footer/></>}/>
         <Route path='/cart' element={<><Header/><Cart/><Footer/></>}/>
         <Route path='/wishlist' element={<><Header/><Wishlist/><Footer/></>}/>
+        <Route path='/order-details' element={<><Header/><Details/><Footer/></>}/>
+        <Route path='/order-success' element={<><Header/><Success/><Footer/></>}/>
       </Routes>
    </Router>
     </>
